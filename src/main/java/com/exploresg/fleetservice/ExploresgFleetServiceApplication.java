d3e657606e3fa0d8af8869c758159ff14c9fc4b1
sree <sreeraj_ec@hotmail.com>
2025-10-03 12:42:05 +0800

    EXPLORE-24: Initial Commit

---
+++ src/main/java/com/exploresg/fleetservice/ExploresgFleetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.exploresg.fleetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploresgFleetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploresgFleetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploresg/fleetservice/ExploresgFleetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploresg.fleetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploresgFleetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
